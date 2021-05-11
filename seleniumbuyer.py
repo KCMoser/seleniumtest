d38b8fb56c898b71076d4012ac208f0b06289a62
KCMoser <email.kylemoser@gmail.com>
2021-05-11 15:09:37 -0400

    Switched to retrying package

--- seleniumbuyer.py
+++ seleniumbuyer.py
@@ -1,12 +1,15 @@
 
 import subprocess
 import pyautogui
+import logging
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 from pathlib import Path
-from retry import retry
+from retrying import retry
+
+logging.basicConfig(filename='tracking.log', level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
 # Read in confidential data and convert to text, stripping out newline char in variables
 f=open('data.txt','r')
@@ -39,17 +42,18 @@ driver.find_element(By.ID, "ap_password").send_keys(pwd)
 time.sleep(1)
 driver.find_element(By.ID, "ap_password").send_keys(Keys.ENTER)
 
+logging.info('retry starts')
 # Here I need to insert the search for the buy button loop with page refresh
-@retry()
+@retry(wait_fixed=2500)         # Retrying, pausing every 2.5 seconds
 def seeker():
-  #try:
+  print('checking begins')
+  logging.info('retrying')
   l= driver.find_element(By.ID, "submit.buy-now")
   #l= driver.find_element(By.ID, "a-autoid-0-announce")
-  print("Button Not Found")
-  #except:
-  #  print("Element does not exist")
+  print("This button is not on the page")
 seeker()
 
+logging.info('retry completes')
 print('Button Found, code continuing')
 #driver.refresh()
 #time.sleep(2)
